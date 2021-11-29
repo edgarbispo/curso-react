96f1887cba4d9ec38b5859fd1e1967bf74400f3f
edgarbispo <edgarbispo@hotmail.com>
2021-11-29 16:37:44 -0300

    18. Estilizando página Main
    Instalar Ícones do React
    npm install react-icons

--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -1,8 +1,23 @@
-import {Title} from './styles'
+import {FaGithub, FaPlus} from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
+
 export default function Main() {
     return(
-        <Title>
-            Main
-        </Title>
+        <Container>
+            <h1>
+                <FaGithub size={25}/>
+                Meus Repositórios
+            </h1>
+
+            <Form onSubmit={()=>{}}>
+                <input type="text"
+                       placeholder={"Adicionar Repositórios"}
+                />
+                <SubmitButton>
+                    <FaPlus color={"FFF"} size={14}/>
+                </SubmitButton>
+            </Form>
+
+        </Container>
     )
 }
