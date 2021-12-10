6e564c82e9c9413fa5ff73efbc4e22fe95dd60af
edgarbispo <edgarbispo@hotmail.com>
2021-12-10 17:36:11 -0300

    24. Carregando detalhes
    // Executar ao mesmo tempo - Axios
    const [repositorioData, issuesData] = await Promise.all([
       api.get(`/repos/${nomeRepo}`),
       api.get(`/repos/${nomeRepo}/issues`)
      ]);

--- repos/src/pages/Repositorio/index.js
+++ repos/src/pages/Repositorio/index.js
@@ -1,12 +1,45 @@
 import {useParams} from "react-router-dom";
+import {Container} from "./styles";
+import {useEffect, useState} from "react";
+import api from '../../services/api';
 
 export default function Repositorio() {
 
+    // Nome do Parametro da Pagina - Router
     const {repositorio} = useParams();
+    const [rep, setRep] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=> {
+
+        async function load() {
+            const nomeRepo = decodeURIComponent(repositorio);
+
+            // Executar ao mesmo tempo - Axios
+            const [repositorioData, issuesData] = await Promise.all([
+                            api.get(`/repos/${nomeRepo}`),
+                            api.get(`/repos/${nomeRepo}/issues`, {
+                                params: {
+                                    state:'open',
+                                    per_page: 5
+                                }
+                            })
+                        ]);
+
+            setRep(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+
+        }
+
+        load();
+
+    }, [repositorio]);
 
     return(
-        <h1 style={{color:'#FFF'}}>
-            { decodeURIComponent(repositorio) }
-        </h1>
+        <Container>
+
+        </Container>
     )
 }
