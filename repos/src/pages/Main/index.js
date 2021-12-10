0cf3ceff7f8d36db4dad3f0ce684f74ab18b7185
edgarbispo <edgarbispo@hotmail.com>
2021-12-10 08:16:02 -0300

    19. Buscando repos
    Instalar Axios
    npm instal axios

--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -1,7 +1,35 @@
 import {FaGithub, FaPlus} from 'react-icons/fa';
 import {Container, Form, SubmitButton} from './styles';
+import {useCallback, useState} from "react";
+
+import api from '../../services/api';
 
 export default function Main() {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([])
+
+    function handleinputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
+    const handleSubmit = useCallback((e) =>{
+        e.preventDefault();
+
+        async function submit() {
+            const response = await api.get(`repos/${newRepo}`);
+            const data = {
+                name: response.data.full_name,
+            }
+
+            setRepositorios([...repositorios, data]);
+            setNewRepo('');
+        }
+
+        submit();
+
+    }, [newRepo, repositorios]);
+
     return(
         <Container>
             <h1>
@@ -9,9 +37,11 @@ export default function Main() {
                 Meus Repositórios
             </h1>
 
-            <Form onSubmit={()=>{}}>
+            <Form onSubmit={handleSubmit}>
                 <input type="text"
                        placeholder={"Adicionar Repositórios"}
+                       value={newRepo}
+                       onChange={handleinputChange}
                 />
                 <SubmitButton>
                     <FaPlus color={"FFF"} size={14}/>
